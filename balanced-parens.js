d887b623595841ddc63d203fd479cd462bd770c3
zeita101 <zeitajb1@gmail.com>
2020-11-18 20:44:19 -0500

    Update balanced-parens.js
    the previous answer did not pass all test

--- balanced-parens.js
+++ balanced-parens.js
@@ -1,17 +1,20 @@
-const str = '()))';
-const balanceParanthesis = str => {
-   let paren = [];
-   for (let i = 0; i < str.length; i++) {
-      if (str[i] === "(") {
-         paren.push(str[i]);
-      } else if (str[i] === ")") {
-         if (paren[paren.length - 1] === "("){
-            paren.pop();
-         }else {
-            paren.push("#");
-         };
-      };
-   }
-   return paren.length;
+let allParens = ['(', '{', '[', ')', '}', ']'];
+
+const parensAreBalanced = (input) => {
+  let parens = [];
+  for(let i = 0; i < input.length; i++) {
+    if (allParens.indexOf(input[i]) > -1) {
+      parens.push(input[i]);
+    }
+  }
+  if (parens.length % 2 !== 0) {
+    return false;
+  }
+  // while(parens.length > 0) {
+  //   if (parens[0] === '(' && parens[1] === ')') {
+  //     parens.splice(0, 2);
+  //   }
+  
+  // }
+  return true;
 }
-console.log(balanceParanthesis(str));
